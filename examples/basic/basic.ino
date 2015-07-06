fc52e711605f563ad3331afbb903a80afc29d665
mgi2212 <danq@quigleys.us>
2015-07-06 15:23:56 -0700

    sample file

---
+++ examples/basic/basic.ino
@@ -0,0 +1,13 @@
+#include <ITimeSource .h>
+#include <NtpPacket.h>
+#include <NtpServer.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
